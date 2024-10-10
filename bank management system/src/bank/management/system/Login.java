f28c063ca0cf08f20757eb34f5603576bcf2af87
Devanand Farkade <115337013+Devanandfarkade@users.noreply.github.com>
2024-10-10 12:40:05 +0530

    Add files via upload
    login page UI created.

---
+++ bank management system/src/bank/management/system/Login.java
@@ -0,0 +1,69 @@
+
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Login extends JFrame implements ActionListener{
+    Login(){
+        setTitle("Automated Teller Machine");
+        setLayout(null);
+        
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+        Image i2=i1.getImage().getScaledInstance(100, 100,Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel lable=new JLabel(i3);
+        lable.setBounds(70, 10, 100, 100);
+        add(lable);
+        
+        JLabel text=new JLabel("Welcome to ATM");
+        text.setFont(new Font("Osward",Font.BOLD,38));
+        text.setBounds(200, 40, 400, 40);
+        add(text);
+        
+         JLabel cardno=new JLabel("Card No:");
+        cardno.setFont(new Font("Raleway",Font.BOLD,28));
+        cardno.setBounds(120, 150, 150, 30);
+        add(cardno);
+        
+        JTextField cardTextField=new JTextField();
+        cardTextField.setBounds(300, 150 , 230 , 30 );
+        add(cardTextField);
+        
+         JLabel pin=new JLabel("PIN:");
+        pin.setFont(new Font("Raleway",Font.BOLD,28));
+        pin.setBounds(120, 220, 250, 30);
+        add(pin);
+        
+         JTextField pinTextField=new JTextField();
+        pinTextField.setBounds(300, 220 , 230 , 30 );
+        add(pinTextField);
+        
+        JButton login=new JButton("SIGN IN");
+        login.setBounds(300, 300, 100, 30);
+        login.setBackground(Color.BLACK);
+        login.setForeground(Color.WHITE);
+        add(login);
+        
+         JButton clear=new JButton("CLEAR");
+        clear.setBounds(430, 300, 100, 30);
+        clear.setBackground(Color.BLACK);
+        clear.setForeground(Color.WHITE);
+        add(clear);
+        
+         JButton signup=new JButton("SIGN UP");
+        signup.setBounds(370, 350, 100, 30);
+        signup.setBackground(Color.BLACK);
+        signup.setForeground(Color.WHITE);
+        add(signup);
+        
+        getContentPane().setBackground(Color.white);
+        setSize(800, 480);
+        setVisible(true);
+        setLocation(300,120);
+    }
+    public static void main(String[] args) {
+     new Login();
+    }
+}
